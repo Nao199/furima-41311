fe03b824db7cc39744fcd80cbab4278a92d40de4
Nao199 <eddionk13@gmail.com>
2024-08-23 17:19:58 +0900

    テストコード記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,7 +14,7 @@ class User < ApplicationRecord
     validates :first_name
     validates :last_name
   end
-
+  
   with_options presence: true, format: { with: /\A[ァ-ヶー]+\z/, message: '全角カタカナを使用してください' } do
     validates :first_name_kana
     validates :last_name_kana
