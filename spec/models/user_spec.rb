05f2cb8ec2a795eaba81f9ae2da8332ac85c5b47
Nao199 <eddionk13@gmail.com>
2024-08-23 19:50:41 +0900

    テストコードパスワード追加

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -11,7 +11,7 @@ RSpec.describe User, type: :model do
         expect(@user).to be_valid
       end 
     end
-    
+
     context '新規登録できないとき' do
       it 'nicknameが空では登録できない' do
         @user.nickname = ''
@@ -73,6 +73,13 @@ RSpec.describe User, type: :model do
         expect(@user.errors.full_messages).to include('Password には英字と数字の両方を含めて設定してください')
       end
 
+      it 'passwordに半角英数字以外が含まれていると登録できない' do
+        @user.password = 'pass word123'
+        @user.password_confirmation = 'pass word123'
+        @user.valid?
+        expect(@user.errors.full_messages).to include('Password には英字と数字の両方を含めて設定してください')
+      end
+      
       it 'first_name が空では登録できない' do
         @user.first_name = ''
         @user.valid?
