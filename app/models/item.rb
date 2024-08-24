a6d595d69a0694531bb8a02f1db1935d2e686dfa
Nao199 <eddionk13@gmail.com>
2024-08-24 16:29:14 +0900

    shipping_dayのactivehash設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,11 +7,12 @@ class Item < ApplicationRecord
   belongs_to :condition
   belongs_to :shipping_fee_status
   belongs_to :prefecture
+  belongs_to :shipping_day
 
   #空の投稿を保存できないようにする
   #validates :title, :text, presence: true
 
   #ジャンルの選択が「---」の時は保存できないようにする
-  validates :category_id, :condition_id, :shipping_fee_status_id, :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :category_id, :condition_id, :shipping_fee_status_id, :prefecture_id, :shipping_day_id, numericality: { other_than: 1 , message: "can't be blank"}
 
 end
