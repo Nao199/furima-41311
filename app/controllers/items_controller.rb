27be0ff067ffff355c81b85568c87e6c63b64a6a
Nao199 <eddionk13@gmail.com>
2024-08-24 15:08:36 +0900

    ActiveHash　設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,16 +4,28 @@ class ItemsController < ApplicationController
   end
 
   def new
-    @items = Item.new
+    @item = Item.new
   end
 
   def create
-    Item.create(item_params)
-    redirect_to '/'
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   private
-  def Item_params
-    params.require(:item).permit(:image, :name, :description).merge(user_id: current_user.id)
+  def item_params
+    params.require(:item).permit(:image, 
+    :name, 
+    :description, 
+    :category_id, 
+    :condition_id,
+    :shipping_fee_status_id,
+    :prefecture_id,
+    :scheduled_delivery_id,
+    :price).merge(user_id: current_user.id)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,14 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+
+  #空の投稿を保存できないようにする
+  #validates :title, :text, presence: true
+
+  #ジャンルの選択が「---」の時は保存できないようにする
+  validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
+
 end
