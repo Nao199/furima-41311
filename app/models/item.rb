6f547ca95528faf56c757b82b4aaa6b9330a892c
Nao199 <eddionk13@gmail.com>
2024-08-24 16:13:08 +0900

    conditionのativehash設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,11 +5,12 @@ class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
   belongs_to :condition
+  belongs_to :shipping_fee_status
 
   #空の投稿を保存できないようにする
   #validates :title, :text, presence: true
 
   #ジャンルの選択が「---」の時は保存できないようにする
-  validates :category_id, :condition_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :category_id, :condition_id, :shipping_fee_status_id, numericality: { other_than: 1 , message: "can't be blank"}
 
 end
